542e6373428c43558cc4a5eea18412d637a464b4
Mike Bussert <mbussert@gmail.com>
2025-07-19 19:30:29 -0400

    Add calendar

--- config/config.js
+++ config/config.js
@@ -9,29 +9,29 @@
  * see https://docs.magicmirror.builders/configuration/introduction.html#enviromnent-variables
  */
 let config = {
-	address: "localhost",	// Address to listen on, can be:
-							// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
-							// - another specific IPv4/6 to listen on a specific interface
-							// - "0.0.0.0", "::" to listen on any interface
-							// Default, when address config is left out or empty, is "localhost"
+	address: "localhost", // Address to listen on, can be:
+	// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
+	// - another specific IPv4/6 to listen on a specific interface
+	// - "0.0.0.0", "::" to listen on any interface
+	// Default, when address config is left out or empty, is "localhost"
 	port: 8080,
-	basePath: "/",	// The URL path where MagicMirror² is hosted. If you are using a Reverse proxy
-									// you must set the sub path here. basePath must end with a /
-	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"],	// Set [] to allow all IP addresses
-									// or add a specific IPv4 of 192.168.1.5 :
-									// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
-									// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
-									// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
+	basePath: "/", // The URL path where MagicMirror² is hosted. If you are using a Reverse proxy
+	// you must set the sub path here. basePath must end with a /
+	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"], // Set [] to allow all IP addresses
+	// or add a specific IPv4 of 192.168.1.5 :
+	// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
+	// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
+	// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
 
-	useHttps: false,			// Support HTTPS or not, default "false" will use HTTP
-	httpsPrivateKey: "",	// HTTPS private key path, only require when useHttps is true
-	httpsCertificate: "",	// HTTPS Certificate path, only require when useHttps is true
+	useHttps: false, // Support HTTPS or not, default "false" will use HTTP
+	httpsPrivateKey: "", // HTTPS private key path, only require when useHttps is true
+	httpsCertificate: "", // HTTPS Certificate path, only require when useHttps is true
 
 	language: "en",
-	locale: "en-US",   // this variable is provided as a consistent location
-			   // it is currently only used by 3rd party modules. no MagicMirror code uses this value
-			   // as we have no usage, we  have no constraints on what this field holds
-			   // see https://en.wikipedia.org/wiki/Locale_(computer_software) for the possibilities
+	locale: "en-US", // this variable is provided as a consistent location
+	// it is currently only used by 3rd party modules. no MagicMirror code uses this value
+	// as we have no usage, we  have no constraints on what this field holds
+	// see https://en.wikipedia.org/wiki/Locale_(computer_software) for the possibilities
 
 	logLevel: ["INFO", "LOG", "WARN", "ERROR"], // Add "DEBUG" for even more logging
 	timeFormat: 24,
@@ -39,7 +39,7 @@ let config = {
 
 	modules: [
 		{
-			module: "alert",
+			module: "alert"
 		},
 		{
 			module: "updatenotification",
@@ -52,7 +52,7 @@ let config = {
 				timeFormat: 12,
 				timezone: "America/New_York",
 				displaySeconds: false,
-				showPeriodUpper: true,
+				showPeriodUpper: true
 			}
 		},
 		{
@@ -60,18 +60,17 @@ let config = {
 			header: "US Holidays",
 			position: "top_left",
 			config: {
+				broadcastPastEvents: true,
 				calendars: [
 					{
 						fetchInterval: 7 * 24 * 60 * 60 * 1000,
 						symbol: "calendar-check",
-						url: "https://ics.calendarlabs.com/76/mm3137/US_Holidays.ics"
+						url: "https://ics.calendarlabs.com/76/mm3137/US_Holidays.ics",
+						color: "red",
+						name: "us_holiday"
 					}
 				]
 			}
-		},
-		{
-			module: "compliments",
-			position: "lower_third"
 		},
 		{
 			module: "weather",
@@ -95,23 +94,26 @@ let config = {
 			}
 		},
 		{
-			module: "newsfeed",
+			module: "MMM-SmartTouch",
+			position: "bottom_center",
+			config: {}
+		},
+		{
+			module: "MMM-CalendarExt3",
 			position: "bottom_bar",
 			config: {
-				feeds: [
-					{
-						title: "New York Times",
-						url: "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml"
-					}
-				],
-				showSourceTitle: true,
-				showPublishDate: true,
-				broadcastNewsFeeds: true,
-				broadcastNewsUpdates: true
+				mode: "month",
+				instanceId: "basicCalendar",
+				locale: "en-US",
+				maxEventLines: 5,
+				firstDayOfWeek: 1,
+				calendarSet: ["us_holiday"]
 			}
-		},
+		}
 	]
 };
 
 /*************** DO NOT EDIT THE LINE BELOW ***************/
-if (typeof module !== "undefined") { module.exports = config; }
+if (typeof module !== "undefined") {
+	module.exports = config;
+}
